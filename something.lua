2619be77bb1055a9bfc52d6f3af695a7a31b8ead
b00Iean <92385804+b00Iean@users.noreply.github.com>
2022-07-11 22:52:05 -0400

    Create something.lua

---
+++ something.lua
@@ -0,0 +1,93 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3534.735, 27.97, 2298.945)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1361.265, 27.97, 2298.945)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1361.265, 27.97, 1990.945)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-3534.735, 27.97, 1990.945)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3534.735, 27.97, 1703.945)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1361.265, 27.97, 1703.945)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1361.265, 27.97, 1395.945)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-3534.735, 27.97, 1395.945)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3534.735, 27.97, 1076.945)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1361.265, 27.97, 1076.945)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1361.265, 27.97, 768.945)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-3534.735, 27.97, 768.945)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3534.735, 27.97, 481.945)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1361.265, 27.97, 481.945)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1361.265, 27.97, 173.945)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-3534.735, 27.97, 173.945)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3534.735, 27.97, -49.055)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1473.265, 27.97, -49.055)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1473.265, 27.97, -357.055)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-3534.735, 27.97, -357.055)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1473.265, 27.97, -644.055)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-3534.735, 27.97, -644.055)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3534.735, 27.97, -952.055)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1473.265, 27.97, -952.055)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1473.265, 27.97, -1192.055)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-3534.735, 27.97, -1192.055)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3534.735, 27.97, -1500.055)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1473.265, 27.97, -1500.055)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1473.265, 27.97, -1787.055)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-3534.735, 27.97, -1787.055)}):Play()
+
+wait(9)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3534.735, 27.97, -2095.055)
+tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(10, Enum.EasingStyle.Linear)
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1473.265, 27.97, -2095.055)}):Play()
