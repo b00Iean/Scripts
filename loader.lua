c2736bbf2e7c1ae5fe0d70323ee725ab1445e99d
b00Iean <92385804+b00Iean@users.noreply.github.com>
2022-07-15 10:58:10 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 1591277193 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/b00Iean/Scripts/main/GothamCity'))()
 elseif game.PlaceId then
-    print('unsupported game detected')
+    print('unsupported game')
 end
