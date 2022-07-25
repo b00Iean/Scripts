c3f99037107820596fc0f4eb10fe0c1b1b39af0a
b00Iean <92385804+b00Iean@users.noreply.github.com>
2022-07-25 16:39:49 -0400

    Delete loader.lua

--- loader.lua
+++
@@ -1,5 +0,0 @@
-if game.PlaceId == 1591277193 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/b00Iean/Scripts/main/GothamCity'))()
-elseif game.PlaceId then
-    warn('unsupported game')
-end
